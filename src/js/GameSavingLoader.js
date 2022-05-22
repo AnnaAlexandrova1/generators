5934767f6a994cc8c7fa0e6fa9d3daafcd5488a2
Anna <anna4776497@gmail.com>
2022-05-22 17:11:13 +0300

    2.1

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,3 +1,4 @@
+import GameSaving from './gameSaving';
 import json from './parser';
 import read from './reader';
 
@@ -5,6 +6,6 @@ export default class GameSavingLoader {
   static async load() {
     const response = await read();
     const result = await json(response);
-    return result;
+    return new GameSaving(JSON.parse(result));
   }
 }
--- src/js/test/app.test.js
+++ src/js/test/app.test.js
@@ -1,7 +1,7 @@
 
 import GameSavingLoader from '../GameSavingLoader';
 
-test('Должна создавать объект типа GameSaving', (done) => {
+test('Должна создавать объект типа GameSaving', () => {
   GameSavingLoader.load().then((saving) => expect(JSON.parse(saving)).toEqual({
     id: 9,
     created: 1546300800,
@@ -9,10 +9,4 @@ test('Должна создавать объект типа GameSaving', (done) 
       id: 1, name: 'Hitman', level: 10, points: 2000,
     },
   }));
-  done();
-});
-
-test('Если promise будет выполнен с ошибкой', () => {
-  expect.assertions(1);
-  return GameSavingLoader.load().catch((err) => expect(err).toMatch('error'));
 });
